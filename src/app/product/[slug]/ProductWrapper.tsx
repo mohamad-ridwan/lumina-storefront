4c6bc4f16333cd24f2f2856e048fa5a5445ab4f7
mohamad-ridwan <mr643062@gmail.com>
2025-11-20 06:46:02 +0700

    setup themeMap

--- src/app/product/[slug]/ProductWrapper.tsx
+++ src/app/product/[slug]/ProductWrapper.tsx
@@ -1,6 +1,6 @@
 import { Shoe } from "@/core/domain/product";
 import { CustomBreadcrumbItem } from "@/shared/components/breadcrumbs/CustomBreadcrumb";
-import { ThemeComponents } from "@/shared/lib/Theme";
+import { ThemeComponent } from "@/shared/lib/Theme";
 import { ActiveProductImg } from "@/shared/types/product";
 
 export type AppProductProps = {
@@ -12,7 +12,7 @@ export type AppProductProps = {
 };
 
 export const ProductWrapper = ({ ...props }: AppProductProps) => {
-  const Component = ThemeComponents<AppProductProps>(`app/Product`);
+  const Component = ThemeComponent<AppProductProps>("theme1", "Product");
 
   return <Component {...props} />;
 };
--- src/shared/lib/Theme.ts
+++ src/shared/lib/Theme.ts
@@ -1,11 +1,27 @@
 import dynamic from "next/dynamic";
-import { ComponentType } from "react";
+// import { ComponentType } from "react";
 
-export const ThemeComponents = <T extends object = Record<string, unknown>>(
-  path: string
-): ComponentType<T> => {
-  const currentTheme: string = "theme2";
-  return dynamic(
-    () => import(`@/themes/${currentTheme}/${path}`)
-  ) as unknown as ComponentType<T>;
+// export const ThemeComponent = <T extends object = Record<string, unknown>>(
+//   path: string
+// ): ComponentType<T> => {
+//   const currentTheme: string = "theme2";
+//   return dynamic(
+//     () => import(`@/themes/${currentTheme}/${path}`)
+//   ) as unknown as ComponentType<T>;
+// };
+
+export const themeMap = {
+  theme1: {
+    Product: dynamic(() => import("@/themes/theme1/app/Product")),
+  },
+  theme2: {
+    Product: dynamic(() => import("@/themes/theme2/app/Product")),
+  },
 };
+
+export function ThemeComponent<T extends object>(
+  theme: keyof typeof themeMap,
+  component: keyof (typeof themeMap)["theme1"]
+) {
+  return themeMap[theme][component] as React.ComponentType<T>;
+}
